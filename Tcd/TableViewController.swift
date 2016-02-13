6adbd36b4891a503787b3a4ac40a7574049713a5
nikrb <nik_rb@yahoo.com>
2016-02-13 03:54:30 +0000

    add save after delete

--- Tcd/TableViewController.swift
+++ Tcd/TableViewController.swift
@@ -144,6 +144,7 @@ class TableViewController: UITableViewController {
             
             // Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            saveItems()
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
